c29b31d90e6a3c76303d7fdd4947edd60b46d540
mabelpertot <mabelgem77@gmail.com>
2026-09-20 18:18:19 -0300

    Actualizando el Endpoint

--- backend/main.py
+++ backend/main.py
@@ -32,9 +32,9 @@ app.add_middleware(
 @app.get("/")
 def read_root():
     return {
-        "status": "online",
-        "equipo": "Equipo N° 4",
-        "message": "Backend FastAPI desplegado exitosamente con Sentry"
+        "Estado": "online",
+        "Equipo": "Equipo N° 4",
+        "Mensaje": "Backend FastAPI desplegado exitosamente con Sentry"
     }
 
 # Endpoint obligatorio para probar Sentry en la exposición en vivo
